ec55643eb8cf0f3822b9b39e3f9301a86ea72d2a
MuffinDev <92362765+MuffinDev9@users.noreply.github.com>
2022-02-19 10:13:44 +0000

    e

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-packages_call = True
-cmdlist = ["kill", "echo"]
-packages = ["call", "package"]
+# Packages
+global packages_call
+packages_call = False
+
+cmdlist = ["kill", "echo", "package"]
+packages = ["call"]
 
 def kill():
     print("Killing...")
 def echo():
     txt = input("text> ")
     print("\"" + txt + "\"")
+    cmd()
+def package():
+    print("What Package Would You Like To Install?")
+    pkg = input("> ")
+    if pkg == "call":
+        packages_call = True
+        print("Call = True")
     cmd()
 
 def cmd():
@@ -15,6 +25,11 @@ def cmd():
         kill()
     elif command == "echo":
         echo()
+    elif command == "package":
+        package()
+    elif command == "call":
+        if packages_call:
+            print("E")
     else:
         print("Command Not Recognised")
         print("Try Again")
